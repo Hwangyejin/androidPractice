5177a073f77e95b80c9817365eb4bd24226f9666
HwangYejin <y1720103@y-y.hs.kr>
2018-03-15 14:24:42 +0900

    remove toolbar and bg

--- app/src/main/java/com/example/bjnproject/myapplication/MainActivity.java
+++ app/src/main/java/com/example/bjnproject/myapplication/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.bjnproject.myapplication;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar); //툴바설정
+        toolbar.setTitleTextColor(Color.parseColor("#ffffff"));
+        setSupportActionBar(toolbar);//액션바와 같게 만들어줌
+
     }
 }
